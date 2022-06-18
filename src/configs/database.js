5ef28426a60e7a6d518d69c75d6fdb9078fb4526
unknown <alzananajmiramadhan@.com>
2022-06-18 09:57:39 +0700

    test

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 module.exports = {
     multipleStatements  : true,
-    host                : "sql6.freemysqlhosting.net",
-    user                : "sql6498920",
-    password            : "atwEmgTqwj",
-    database            : "sql6498920"
+    host                : "sql5.freemysqlhosting.net",
+    user                : "sql5500540",
+    password            : "i1ViQJ22fx",
+    database            : "sql5500540"
 };
